46972a7df65dd703e849f612e928e088076172a7
rezaulraj <rezaul.coderpro@gmail.com>
2023-09-03 00:09:29 +0600

    local blog

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,55 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [name, setName] = useState('harsh');
+
+  const handelClick = () => {
+    setName('Rezaul');
+  };
+
+  const [count, setNumber] = useState(0);
+  const increseNumber = () => {
+    setNumber(count + 1);
+  };
+  const decrementNumber = () => {
+    if (count > 0) setNumber(count - 1);
+  };
+
   return (
-    <div className='home'>
+    <div className="home">
       <h2>HomePage</h2>
+      <p>{name}</p>
+
+      <button onClick={handelClick}>Click me</button>
+      <br />
+      <br />
+      <button
+        onClick={decrementNumber}
+        style={{
+          border: '3px solid red',
+          background: '#333',
+          color: 'white',
+        }}
+      >
+        -
+      </button>
+      <span
+        style={{
+          padding: '0 20px',
+        }}
+      >
+        {count}
+      </span>
+      <button
+        onClick={increseNumber}
+        style={{
+          border: '3px solid green',
+          background: '#333',
+          color: 'white',
+        }}
+      >
+        +
+      </button>
     </div>
   );
 };
